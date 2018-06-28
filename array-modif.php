7b4e260024c7d109515f06771dedd41eaa0607d6
Dewi Maliana <dewimaliana04@gmail.com>
2018-06-28 14:09:09 +0700

    praktek2

---
+++ array-modif.php
@@ -0,0 +1,12 @@
+<?php
+$data = array(
+    "nama" => "dewi maliana",
+    "usia" => 20,
+    "jomblo" => true,
+    0.1 => 5
+);
+echo "NAMA : " . $data["nama"] . "<br>";
+echo "USIA : " . $data["usia"] . "<br>";
+echo "STATUS JOMBLO : " . $data["jomblo"] . "<br>";
+echo "data lain : " . $data[0.1];
+?>
